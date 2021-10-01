477776c38a370648616b41c60a139b5b3f0d63e8
ChristinaGaitan <christina.gaitan@gmail.com>
2021-10-01 12:42:06 -0500

    Use useReducer for handeling email value and validity

--- src/components/Login/Login.js
+++ src/components/Login/Login.js
@@ -1,43 +1,83 @@
-import React, { useState, useEffect } from 'react';
+import React, { useState, useEffect, useReducer } from 'react';
 
 import Card from '../UI/Card/Card';
 import classes from './Login.module.css';
 import Button from '../UI/Button/Button';
 
+
+// When to use useReducer
+// States that belong together
+// (enteredEmail, emailIsValid)
+// Update state that depends of other states
+// setEmailIsValid(enteredEmail.includes('@'));
+
+const emailReducer = (state, action) => {
+  if(action.type === 'USER_INPUT') {
+    return {
+      value: action.value,
+      isValid: action.value.includes('@')
+    }
+  }
+
+  if(action.type === 'INPUT_BLUR') {
+    return {
+      value: state.value,
+      isValid: state.value.includes('@')
+    }
+  }
+
+  return {
+    value: '',
+    isValid: false
+  }
+}
+
 const Login = (props) => {
-  const [enteredEmail, setEnteredEmail] = useState('');
-  const [emailIsValid, setEmailIsValid] = useState();
+  // const [enteredEmail, setEnteredEmail] = useState('');
+  // const [emailIsValid, setEmailIsValid] = useState();
   const [enteredPassword, setEnteredPassword] = useState('');
   const [passwordIsValid, setPasswordIsValid] = useState();
   const [formIsValid, setFormIsValid] = useState(false);
 
-  useEffect(() => {
-    // Debouncing
-    const identifier = setTimeout(() => {
-      console.log('Checking validity!');
-      setFormIsValid(
-        enteredEmail.includes('@') && enteredPassword.trim().length > 6
-      )
-    }, 500);
+  const [emailState, dispatchEmail] = useReducer(emailReducer, { value: '', isValid: null });
 
-    // Cleanup function
-    // Runs before every execution
-    return () => {
-      console.log('Cleaning!');
-      clearTimeout(identifier);
-    };
-  }, [enteredEmail, enteredPassword])
+  // useEffect(() => {
+  //   // Debouncing
+  //   const identifier = setTimeout(() => {
+  //     console.log('Checking validity!');
+  //     setFormIsValid(
+  //       enteredEmail.includes('@') && enteredPassword.trim().length > 6
+  //     )
+  //   }, 500);
+
+  //   // Cleanup function
+  //   // Runs before every execution
+  //   return () => {
+  //     console.log('Cleaning!');
+  //     clearTimeout(identifier);
+  //   };
+  // }, [enteredEmail, enteredPassword])
+
 
   const emailChangeHandler = (event) => {
-    setEnteredEmail(event.target.value);
+    // Object with type and value is the action that we send to emailReducer
+    console.log('==== event.target.value', event.target.value)
+    dispatchEmail({ type: 'USER_INPUT', value: event.target.value });
+
+    setFormIsValid(
+      event.target.value.includes('@') && enteredPassword.trim().length > 6
+    )
   };
 
   const passwordChangeHandler = (event) => {
     setEnteredPassword(event.target.value);
+    setFormIsValid(
+      emailState.isValid && event.target.value.trim().length > 6
+    )
   };
 
   const validateEmailHandler = () => {
-    setEmailIsValid(enteredEmail.includes('@'));
+    dispatchEmail({ type: 'INPUT_BLUR' });
   };
 
   const validatePasswordHandler = () => {
@@ -46,7 +86,7 @@ const Login = (props) => {
 
   const submitHandler = (event) => {
     event.preventDefault();
-    props.onLogin(enteredEmail, enteredPassword);
+    props.onLogin(emailState.value, enteredPassword);
   };
 
   return (
@@ -54,14 +94,14 @@ const Login = (props) => {
       <form onSubmit={submitHandler}>
         <div
           className={`${classes.control} ${
-            emailIsValid === false ? classes.invalid : ''
+            emailState.isValid === false ? classes.invalid : ''
           }`}
         >
           <label htmlFor="email">E-Mail</label>
           <input
             type="email"
             id="email"
-            value={enteredEmail}
+            value={emailState.value}
             onChange={emailChangeHandler}
             onBlur={validateEmailHandler}
           />
